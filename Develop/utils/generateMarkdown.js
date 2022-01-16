d8c5c8770fd6b4d266a466f66d7783b029e2c323
tooqk4u <tooqk4u@protonmail.com>
2022-01-16 16:57:50 -0500

    create const for badges and licence links

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,20 @@
+// creat const for badges
+const badges = {
+  "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  "Apache": "[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  "Mozilla": "[![License: Mozilla](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+  "Unlicense": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+};
+
+// create const for license sites
+const licenseLinks = {
+  "MIT": "https://opensource.org/licenses/MIT",
+  "Apache": "https://opensource.org/licenses/Apache-2.0",
+  "Mozilla": "https://opensource.org/licenses/MPL-2.0",
+  "Unlicense": "http://unlicense.org/"
+};
+
+
 // function that returns a license badge based on which license is passed in if there is no license, an empty string is returned
 function renderLicenseBadge(license) {}
 
